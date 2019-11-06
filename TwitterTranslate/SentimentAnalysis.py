93ee7f5066d7c6b9b9121b15a77c64650c2151e2
Dan Haub <haub@chapman.edu>
2019-11-06 11:28:42 -0800

    made new branch

---
+++ TwitterTranslate/SentimentAnalysis.py
@@ -0,0 +1,10 @@
+#from googletrans import Translator
+
+#ParsedTweet{text: "string", lang: "string"}
+
+tweet = {'text': "hello world", 'lang': "en"}
+
+print(tweet['text'])
+print(tweet['lang'])
+
+input()
