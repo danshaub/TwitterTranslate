5fc2c4e937cadd7708beafd8bc8e2c59ab8c02b5
Dan Haub <haub@chapman.edu>
2019-11-05 17:27:00 -0800

    Added basic translation implementation

--- TwitterTranslate/TweetDisplay.py
+++ TwitterTranslate/TweetDisplay.py
@@ -2,14 +2,18 @@ import twitter
 import sys
 import codecs
 import json
+from googletrans import Translator
 
 
 sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
-OAUTH_TOKEN = ''
-OAUTH_TOKEN_SECRET = ''
+translator = Translator()
+print(translator.detect('No quiero estar en clase hoy.'))
+
+CONSUMER_KEY = 'gKkxTnXxqn3v3tFjrfu9oZeYB'
+CONSUMER_SECRET = 'HDomCUoqsy933LKptgRtMcNtOzo0UaY3ML8OGmiV375mdL1NNH'
+OAUTH_TOKEN = '702034289-1w5CFub2H5ZR7EOol3OWyb2Yq6NQrFjACos7gr6O'
+OAUTH_TOKEN_SECRET = '5EXFQg8vIaL6xBr6YTsoewvxjwa7Z9QL36daBfP0OLJcz'
 
 auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
                            CONSUMER_KEY, CONSUMER_SECRET)
@@ -59,6 +63,6 @@ words = [w
 for i in range(0,5):
     print(json.dumps(status_texts[i], indent=1))
     print()
-=======
+
 input()
 
